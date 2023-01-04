86856c60fcca0c0b34fc6fdd0cb84199f3052006
SangDon <adoim@naver.com>
2023-01-04 21:32:25 +0900

    Feat: 메뉴바 위치선정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import tw from 'tailwind-styled-components';
+import Menu from '../components/menu';
 const Container = tw.div`
   flex
   flex-row
@@ -14,7 +15,11 @@ const Layout = tw.div`
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
-      <Layout>my-blog</Layout>
+      <Layout>
+        my-blog
+        <br />
+        <Menu></Menu>
+      </Layout>
       {/* //todo: 클릭시 홈페이지로 이동 */}
       <Component {...pageProps} />
     </Container>
