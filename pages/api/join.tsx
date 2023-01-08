ef91e43128bdd20c1896df1458a81032310d7618
SangDon <adoim@naver.com>
2023-01-09 04:03:58 +0900

    Feat: prisma / planetscale 기본 연결 구성

--- pages/api/join.tsx
+++ pages/api/join.tsx
@@ -1,5 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import client from '../../libs/client';
 
-export default function Join(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ ok: true });
+export default async function Join(req: NextApiRequest, res: NextApiResponse) {
+  const createdUser = await client.user.create({
+    data: {
+      email: 'adoim@naver.com',
+      name: 'ado',
+      password: '12345',
+    },
+  });
+
+  return res.status(200).json(createdUser);
 }
