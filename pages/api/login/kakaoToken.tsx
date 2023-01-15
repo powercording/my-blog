15685663cfae5fd9bec851ff0ad8f5e3f59ce86a
powercording <adoim@naver.com>
2023-01-15 10:43:18 +0900

    Feat: 카카오 API 에서 사용자 엑세스 토큰 발급 완료

--- pages/api/login/kakaoToken.tsx
+++ pages/api/login/kakaoToken.tsx
@@ -5,18 +5,24 @@ export async function kakaoToken(req: NextApiRequest, res: NextApiResponse) {
   const url = `https://kauth.kakao.com/oauth/token`;
   const code = req.query.code;
 
-  const kakaoToken = await fetch(url, {
+  const params = new URLSearchParams();
+  params.append('grant_type', 'authorization_code');
+  params.append('client_id', process.env.NEXT_PUBLIC_KAKAO_REST + '');
+  params.append('redirect_uri', process.env.NEXT_PUBLIC_KAKAO_REDIRECT + '');
+  params.append('code', code + '');
+
+  const fetchOptions = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
-    body: `grant_type=authorization_code&
-      client_id=${process.env.NEXT_PUBLIC_KAKAO_REST}&
-      redirec_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT}&
-      code=${code}`,
-  }).then(res => res.json());
+    body: params,
+  };
+
+  const kakaoToken = await fetch(url, fetchOptions).then(res => res.json());
   console.log(kakaoToken);
-  res.status(200).redirect('/login');
+
+  res.status(302).redirect('/login');
 }
 
 export default apiHandler('GET', kakaoToken);
