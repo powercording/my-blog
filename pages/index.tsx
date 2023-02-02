c4eb7ee37cc31aac644475c49037428f73299ed8
SangDon <adoim@naver.com>
2023-02-02 21:05:17 +0900

    Style: main 페이지 레이아웃틀 작업

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,36 @@ import useUser from '@libs/client/useUser';
 import tw from 'tailwind-styled-components';
 
 const Container = tw.div`
-  flex;
+  flex-col
+  h-full
 `;
 const WelcomeWord = tw.h1`
+  font-extrabold
+`;
+
+const CardContainer = tw.div`
+  flex h-full gap-2
+`;
+
+const Card = tw.div`
+  box-border flex flex-col p-5 gap-5
+  w-80 h-full bg-white border-gray-300 border-2 rounded-lg
+  hover:cursor-pointer shadow-slate-400 shadow-md
+  relative items-start
+`;
+
+const Img = tw.img`
+  w-full rounded-lg
+  h-full blur-sm
+`;
+
+const Discription = tw.div`
+  absolute top-[5%] bottom-[5%] left-[8%] right-[8%] flex 
+  flex-col justify-end
+  items-start
+`;
+
+const Subject = tw.h1`
   font-extrabold
 `;
 
@@ -14,6 +41,26 @@ export default function Home() {
   return (
     <Container>
       <WelcomeWord>Home.index</WelcomeWord>
+      <CardContainer>
+        <Card>
+          <Img></Img>
+          <Discription>
+            <Subject>dd</Subject>
+          </Discription>
+        </Card>
+        <Card>
+          <Img></Img>
+          <Discription>
+            <Subject>dd</Subject>
+          </Discription>
+        </Card>
+        <Card>
+          <Img></Img>
+          <Discription>
+            <Subject>dd</Subject>
+          </Discription>
+        </Card>
+      </CardContainer>
     </Container>
   );
 }
