4170c7920331a4d6af18da350b76c93e5eccbe81
powercording <adoim@naver.com>
2023-02-03 18:36:35 +0900

    Style: main 화면 post 디스플레이 스타일링

---
+++ components/Post.tsx
@@ -0,0 +1,41 @@
+import tw from 'tailwind-styled-components';
+
+const Card = tw.div`
+  box-border flex flex-col p-5 gap-5
+  w-80 h-full bg-white border-gray-300 border-2 rounded-xl
+  hover:cursor-pointer shadow-slate-400 shadow-md
+  absolute items-start flex-shrink-0 left-14
+`;
+
+const Img = tw.img`
+  w-full rounded-lg
+  h-full blur-sm
+`;
+
+const Discription = tw.div`
+  absolute top-[5%] bottom-[5%] left-[8%] right-[8%] flex 
+  flex-col justify-end
+  items-start
+`;
+
+const Subject = tw.h1`
+  font-extrabold
+`;
+
+type PostType = {
+  sub: string;
+  [key: string]: any;
+};
+
+export default function Post({ sub, ...rest }: PostType) {
+  return (
+    <>
+      <Card {...rest}>
+        <Img />
+        <Discription>
+          <Subject>{sub}</Subject>
+        </Discription>
+      </Card>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,59 @@
+import Post from '@components/Post';
 import useUser from '@libs/client/useUser';
+import { useEffect } from 'react';
 import tw from 'tailwind-styled-components';
 
 const Container = tw.div`
   flex-col
   h-full
+  w-full
 `;
 const WelcomeWord = tw.h1`
   font-extrabold
 `;
 
 const CardContainer = tw.div`
-  flex h-full gap-2
+  overflow-x-hidden relative rounded-xl
+  flex h-full gap-2 w-[50%]
 `;
 
-const Card = tw.div`
-  box-border flex flex-col p-5 gap-5
-  w-80 h-full bg-white border-gray-300 border-2 rounded-lg
-  hover:cursor-pointer shadow-slate-400 shadow-md
-  relative items-start
-`;
-
-const Img = tw.img`
-  w-full rounded-lg
-  h-full blur-sm
-`;
-
-const Discription = tw.div`
-  absolute top-[5%] bottom-[5%] left-[8%] right-[8%] flex 
-  flex-col justify-end
-  items-start
-`;
-
-const Subject = tw.h1`
-  font-extrabold
-`;
+const test = [
+  { sub: '이상돈', id: 1 },
+  { sub: '김종찬', id: 2 },
+  { sub: '심규영', id: 3 },
+  { sub: '천왕역', id: 4 },
+  { sub: '오류동역', id: 5 },
+];
 
 export default function Home() {
   const user = useUser();
-  console.log(user);
+
+  const postSize = 5;
+  let currentPost = 1;
+
+  useEffect(() => {
+    const posts = document.querySelectorAll('#post');
+    const slideWidth = posts[0].clientWidth;
+    console.log(slideWidth);
+    // posts[0].classList.add('left-2');
+    posts.forEach((post, index) => {
+      if (index === 0) return;
+      post.setAttribute('style', `left:${slideWidth * index - 20 * index}px`);
+    });
+  }, []);
+
   return (
     <Container>
-      <WelcomeWord>Home.index</WelcomeWord>
+      <WelcomeWord>Post</WelcomeWord>
       <CardContainer>
-        <Card>
-          <Img></Img>
-          <Discription>
-            <Subject>dd</Subject>
-          </Discription>
-        </Card>
-        <Card>
-          <Img></Img>
-          <Discription>
-            <Subject>dd</Subject>
-          </Discription>
-        </Card>
-        <Card>
-          <Img></Img>
-          <Discription>
-            <Subject>dd</Subject>
-          </Discription>
-        </Card>
+        {test.map(post => (
+          <Post
+            key={post.id}
+            sub={post.sub}
+            id={'post'}
+            className={'card'}
+          ></Post>
+        ))}
       </CardContainer>
     </Container>
   );
